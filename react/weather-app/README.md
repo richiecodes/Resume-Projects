60b51f2bc43f7a117fba6593cfaf9f6c9c6cf17a
Richard DeLuca <richard_deluca@outlook.com>
2021-03-16 19:41:14 -0400

    First commit -- Weather Application

---
+++ react/weather-app/README.md
@@ -0,0 +1,6 @@
+Basic weather app w/ dynamic backgrounds based on city's temperature
+
+v0.0.1
++Created weather app
++Basic CSS styling
+*No functionality as of yet*
---
+++ react/weather-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const App = () => {
+  return(
+    <div className="App">
+      <h1>Weather App</h1>
+      <div className="weather-card">
+        {/* WORK HERE */}
+        <input type="text" placeholder="search..." />
+        <p>Date, Live time counter</p>
+        <p>City, State</p>
+        <p>Weather Icon</p>
+        <p>Weather</p>
+        <p>Radio button -- Toggle between F and C degrees</p>
+      </div>
+    </div>
+  )
+}
+
+export default App;
