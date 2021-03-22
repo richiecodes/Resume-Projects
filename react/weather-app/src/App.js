d02bda47d4f2ea24343471b64e1208f7baf3d9ce
Richard DeLuca <richard_deluca@outlook.com>
2021-03-22 18:33:39 -0400

    ok i really broke it now lol

--- react/weather-app/src/App.js
+++ react/weather-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 const API = {
   key: "11ee13fd472e951dfa116fd454568e46",
@@ -9,25 +8,40 @@ const API = {
 const App = () => {
   
   let date = new Date().toDateString();
+  let degree = "F";
   
   const [weather, setWeather] = useState({});
-  const [units, setUnits] = useState("imperial");
   const [query, setQuery] = useState('');
+  let [isCelsius, setIsCelsius] = useState();
+  let [temperature, setTemp] = useState();
 
   const search = evt => {
     if (evt.key === "Enter") {
-      fetch(`${API.base}weather?q=${query}&units=${units}&APPID=${API.key}`)
+      fetch(`${API.base}weather?q=${query}&APPID=${API.key}`)
         .then(res => res.json())
         .then(result => {
           setWeather(result);
+          setTemp(result.main.temp);
           setQuery('');
           console.log(result);
+          console.log(temperature);
         });
     }
   }
 
+  function convertTemp(t) {
+    if(!isCelsius) {
+      setTemp((t - 273.15) * (9/5) - 32);
+    } else {
+      setTemp(t - 273.15);
+    }
+  }
+
+  if(isCelsius ? degree = 'C' : degree = 'F');
+
+
   return (
-      <div className={(typeof weather.main != "undefined") ? ((weather.main.temp >= 50) ? 'app warm' : 'app') : 'app'}>
+      <div className={(typeof weather.main != "undefined") ? ((temperature >= 50) ? 'app warm' : 'app') : 'app'}>
         <main>
           <div className="search-box">
             <input 
@@ -46,8 +60,8 @@ const App = () => {
               <div className="date">{date}</div>
             </div>
             <div className="weather-box">
-              <div className="temp">
-                {Math.round(weather.main.temp)}°F
+              <div className="temp" onMouseOver={setIsCelsius = true}>
+                {Math.round(temperature)}°{degree} 
               </div>
               <div className="weather">{weather.weather[0].main}</div>
               <div className="weather-desc">({weather.weather[0].description})</div>
